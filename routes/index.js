e32ae8711347731f14fe7bdcfa78ee9b555a4043
Neil Bedi <neilbedi@gmail.com>
2015-08-10 19:04:42 -0400

    basic request to superquery

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var test;
+  request('https://db-superproxy.appspot.com/query?id=ag9zfmRiLXN1cGVycHJveHlyFQsSCEFwaVF1ZXJ5GICAgICAgIAKDA', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      test = JSON.parse(body);
+      console.log(test["rows"]);
+      res.render('index', { title: 'Express', test: test["rows"] });
+    }
+  })
 });
 
 module.exports = router;
