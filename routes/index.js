5fb7895d0c3d58bfee26f04a246b2baa39f51e3c
Neil Bedi <neilbedi@gmail.com>
2015-08-19 23:04:31 -0400

    realtime readded, no functionality

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,10 @@ router.get('/1year', function(req, res, next) {
   res.render('index', { title: "1 year", update: "monthly", javascript: "1year.js" });
 });
 
+router.get('/realtime', function(req, res, next) {
+  res.render('index', { title: "Real time", update: "in real time", javascript: "realtime.js" });
+});
+
 router.get('/custom', function(req, res, next) {
   res.render('index', { title: "Custom query", update: "daily", javascript: "custom.js" });
 });
